6995a9d612d71fee3b35d1736637c10729cf5b58
sujal-aggarwal <sujalrockstar10@gmail.com>
2024-01-09 13:39:24 +0530

    Saving changes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
-const express=require("express");
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app=express();
 
-app.listen(5000,()=>{
-    console.log(`Connected to Port ${5000}`);
-});
+mongoose
+    .connect(
+        `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.qhk2alf.mongodb.net/?retryWrites=true&w=majority`
+    ).then(()=>
+        app.listen(5000,()=>
+            console.log("Connected to Database and Server is Running")
+        )
+    )
+    .catch((e)=>(console.log(e)));
